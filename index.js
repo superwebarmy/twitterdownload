79fcd97fc7387d64e46fda0849b8f336d9ed97d8
shakti goyal <shaktigoyal@192.168.1.7>
2023-04-01 18:52:40 +0530

    first commit

--- index.js
+++ index.js
@@ -1,49 +1,24 @@
-const puppeteer = require('puppeteer');
 const express = require('express');
 const bodyParser = require('body-parser');
-
 const app = express();
+const { TwitterScraper } = require("@tcortega/twitter-scraper");
 
 app.use(bodyParser.json());
 
-const PORT = process.env.PORT || 5001; 
+const PORT = 5001; 
 
 
-async function getHashtag(keyword){
-    const browser = await puppeteer.launch({headless: true});
-    const page = await browser.newPage();
-    await page.goto('https://www.all-hashtag.com/hashtag-generator.php');
-    await page.waitForSelector('#keyword');
-    await page.type('#keyword', keyword);
-    await page.waitForSelector('.btn-gen');
-    await page.click('.btn-gen');
-    await page.waitForSelector('#copy-hashtags');
-    const hash = await page.evaluate(()=>{
-        return document.querySelector('#copy-hashtags').textContent;
-    });
-    const array = hash.split(' ');
-    const objectArray = array.map((e)=>({
-        hashtag: e
-    }));
-    browser.close();
-    return objectArray;
-}
-
-
-
-
-app.post('/api', async (req,res)=>{
-    const hashtagKeyword = req.body.hashtagKeyword;
-    if(!hashtagKeyword){
-        res.status(400);
-        res.send({
-            error: 'please pass the required parameter in the body'
-        });
-    } else {
-        const hashtagArray = await getHashtag(hashtagKeyword);
-        res.status(200);
-        res.send(hashtagArray);
+app.post('/twitterdownload', async (req,res)=>{
+    const link = req.body.link;
+    
+    try{
+        const twtScraper = await TwitterScraper.create();
+        const tweetMeta = await twtScraper.getTweetMeta(link);
+        res.send(tweetMeta);
+    } catch(e){
+        res.status(500).send({error: 'error'});
     }
+
 });
 
 
